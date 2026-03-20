541bd16a53d0329ddd699577a1cf3e029d25168d
Tanishk405 <your-email@example.com>
2026-03-20 16:19:30 +0530

    small changes

--- Backend/app.js
+++ Backend/app.js
@@ -13,7 +13,7 @@ const PORT = 3000;
 
 app.use(cookieParser())
 app.use(cors({
-    origin: 'https://cat-gbt-xhx7.vercel.app/',
+    origin: 'https://cat-gbt-xhx7.vercel.app',
     credentials: true
 }));
 app.use(express.json())
