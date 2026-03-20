78192ab1ba82249ec40c482fcb746ea61afcdafa
Tanishk405 <your-email@example.com>
2026-03-20 16:25:56 +0530

    cross origin

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -74,7 +74,7 @@ export const Login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: false,
-            sameSite: 'lax',
+            sameSite: 'none',
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
